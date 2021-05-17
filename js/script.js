73b910d25e7b4853e43ddd39b1b161e37350834c
Krzysztof Jaczewski <krzysztofjaczewski87@gmail.com>
2021-05-17 09:11:13 +0200

    created gates inner ring

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
-const gateSymbols = document.querySelectorAll(".gates__symbol");
 
-const rotateGateSymbols = () =>{
-gateSymbols.forEach((element,index) => {
-        const angle = 9.23076923077;
-        element.style.transform = "rotate("+ (angle*index)+"deg)";
-});
+const placeGateChevrons = (n, rx, ry, so, cw) =>{
+        const gateChevrons = document.querySelectorAll(".gates__chevron");
+        gateChevrons.forEach((element, index) => {
+                const angle = 9.23076923077;
+                element.style.transform = "rotate(" + (angle * index) + "deg)";
+                element.style.top = String(ry + -ry * Math.cos((360 / n / 180) * (index + so) * Math.PI)) + 'px';
+                element.style.left = String(rx + rx * (cw ? Math.sin((360 / n / 180) * (index + so) * Math.PI) : -Math.sin((360 / n / 180) * (index + so) * Math.PI))) + 'px';
+ 
+        });
+     
+};
+
+const init = () => {
+        placeGateChevrons(39, 200, 200, 0, true);
 }
 
-rotateGateSymbols();
+init();
